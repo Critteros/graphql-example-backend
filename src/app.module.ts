d265fc1713c2c10480ef28828f910bdae31751f7
Szymon Kozioł <szymonkoziol3@gmail.com>
2025-02-03 00:37:22 +0100

    GraphQL initial setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // GraphQLModule.forRoot<MercuriusDriverConfig>({
+    //   driver: MercuriusDriver,
+    //   graphiql: true,
+    // }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            colorize: true, // Enable colors in logs
+          },
+        },
+      },
+    }),
+  );
   await app.listen(process.env.PORT ?? 8080);
 }
 void bootstrap();
